8f3044e57ab1ec21623853d3258876743d21875a
Naina Kumar <125777302+NainaMKumar@users.noreply.github.com>
2025-12-31 02:18:58 +0530

    delete Ad Banner

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-import AdBanner from '../components/AdBanner';
 import Hero from '../components/Hero';
 import Features from '../components/Features';
 import HowItWorks from '../components/HowItWorks';
